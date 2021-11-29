0e846bf3863da963538aa415fe58c43a8dc65a74
Olatunji Olayemi <olayemi289@gmail.com>
2021-11-30 00:39:48 +0100

    corrected midone admin

--- stubs/app/Models/Notification.php
+++ stubs/app/Models/Notification.php
@@ -2,7 +2,7 @@
 
 namespace App\Models;
 
-// use App\Mail\NewProject;
+
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Mail;
